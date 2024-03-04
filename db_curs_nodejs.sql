a505ce0ecc54191d6f445dfaf191cf0dd9254e13
G. Esteban García <g.estebangarcia@iMac-de-G.local>
2024-03-05 00:58:24 +0100

    bbdd

--- db_curs_nodejs.sql
+++ db_curs_nodejs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-03-2024 a las 06:27:55
+-- Tiempo de generación: 05-03-2024 a las 00:53:42
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -64,10 +64,40 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `start`, `end`) VALUES
-(1, 'aniversario', '2024-01-30 11:00:00', '2024-01-30 20:00:00'),
-(3, 'rebajas', '2024-01-20 11:13:36', '2024-01-20 22:13:36'),
-(4, 'Hola Isma!', '2024-02-15 11:00:00', '2024-02-29 12:00:00'),
-(5, 'Reunión', '2024-02-05 12:00:00', '2024-02-05 17:00:00');
+(8, 'Semana Santa', '2024-03-23 23:00:00', '2024-03-30 23:00:00'),
+(9, 'Día Internacional para Concienciar sobre el Desarme y la No Proliferación', '2024-03-04 23:00:00', '2024-03-05 22:59:00'),
+(10, 'Semana Мundial de la Armonía Interconfesional', '2024-01-31 23:00:00', '2024-02-07 22:59:00'),
+(11, 'Día Internacional de la Tolerancia Cero con la Mutilación Genital Femenina', '2024-02-05 23:00:00', '2024-02-06 22:59:00'),
+(12, 'Día Mundial de los Humedales', '2024-02-01 23:00:00', '2024-02-02 22:59:00'),
+(13, 'Día Internacional de la Fraternidad Humana', '2024-02-03 23:00:00', '2024-02-04 22:59:00'),
+(14, 'Día Internacional del Leopardo Árabe', '2024-02-09 23:00:00', '2024-02-10 22:59:00'),
+(15, 'Día Mundial de las Legumbres', '2024-02-09 23:00:00', '2024-02-10 22:59:00'),
+(16, 'Día Internacional de la Mujer y la Niña en la Ciencia', '2024-02-10 23:00:00', '2024-02-11 22:59:00'),
+(17, 'Día Internacional para la Prevención del Extremismo Violento cuando Conduzca al Terrorismo', '2024-02-11 23:00:00', '2024-02-12 22:59:00'),
+(18, 'Día Mundial de la Radio', '2024-02-12 23:00:00', '2024-02-13 22:59:00'),
+(19, 'Día Mundial de la Resiliencia del Turismo', '2024-02-16 23:00:00', '2024-02-17 22:59:00'),
+(20, 'Día Mundial de la Justicia Social', '2024-02-19 23:00:00', '2024-02-20 22:59:00'),
+(21, 'Día Internacional de la Lengua Materna', '2024-02-20 23:00:00', '2024-02-21 22:59:00'),
+(22, 'Día Mundial de los Pastos Marinos', '2024-02-29 23:00:00', '2024-03-01 22:59:00'),
+(23, 'Día de la Cero Discriminación (ONUSIDA)', '2024-02-29 23:00:00', '2024-03-01 22:59:00'),
+(24, 'Día Mundial de la Vida Silvestre', '2024-03-02 23:00:00', '2024-03-03 22:59:00'),
+(25, 'Día Internacional de la Mujer', '2024-03-07 23:00:00', '2024-03-08 22:59:00'),
+(26, 'Día Internacional de las Juezas', '2024-03-09 23:00:00', '2024-03-10 22:59:00'),
+(27, 'Día Internacional para Combatir la Islamofobia', '2024-03-14 23:00:00', '2024-03-15 22:59:00'),
+(28, 'Día de la lengua francesa', '2024-03-19 23:00:00', '2024-03-20 22:59:00'),
+(29, 'Día Internacional de la Felicidad', '2024-03-19 23:00:00', '2024-03-20 22:59:00'),
+(30, 'Día Mundial del Agua', '2024-03-21 23:00:00', '2024-03-22 22:59:00'),
+(31, 'Día Meteorológico Mundial (OMM)', '2024-03-22 23:00:00', '2024-03-23 22:59:00'),
+(32, 'Día Internacional del Derecho a la Verdad en relación con Violaciones Graves de los Derechos Humanos y de la Dignidad de las Víctimas', '2024-03-23 23:00:00', '2024-03-24 22:59:00'),
+(33, 'Día Mundial de la Tuberculosis (OMS)', '2024-03-23 23:00:00', '2024-03-24 22:59:00'),
+(34, 'Día Internacional de Cero Desechos', '2024-03-29 23:00:00', '2024-03-30 22:59:00'),
+(35, 'Día internacional de Solidaridad con los miembros del personal detenidos o desaparecidos', '2024-03-24 23:00:00', '2024-03-25 22:59:00'),
+(36, 'Día Internacional de Recuerdo de las Víctimas de la Esclavitud y la Trata Transatlántica de Esclavos', '2024-03-24 23:00:00', '2024-03-25 22:59:00'),
+(37, 'Día Internacional de la Eliminación de la Discriminación Racial', '2024-03-20 23:00:00', '2024-03-21 22:59:00'),
+(38, 'Día Mundial de la Poesía', '2024-03-20 23:00:00', '2024-03-21 22:59:00'),
+(39, 'Día Internacional del Naw-Rúz', '2024-03-20 23:00:00', '2024-03-21 22:59:00'),
+(40, 'Día Mundial del Síndrome de Down', '2024-03-20 23:00:00', '2024-03-21 22:59:00'),
+(41, 'Día Internacional de los Bosques', '2024-03-20 23:00:00', '2024-03-21 22:59:00');
 
 -- --------------------------------------------------------
 
@@ -147,13 +177,14 @@ CREATE TABLE `locations` (
 
 INSERT INTO `locations` (`id`, `name`, `latitude`, `longitude`) VALUES
 (2, 'Madrid', 40.41650000, -3.70256000),
-(4, 'Carrer Aragó', 41.40566422, 2.18275522),
-(5, 'Bau', 41.39755850, 2.19603527),
-(6, 'Casa Paca', 41.39964218, 2.16042977),
-(7, 'Sants-Montjuïc', 41.36484325, 2.14957073),
-(22, 'Plaza de Toros', 41.40008699, 2.18125820),
-(25, 'Plaza Tetuán', 41.39496839, 2.17557192),
-(27, 'Plaza de Nuestra Señora del Pilar', 41.65639299, -0.87892771);
+(25, 'Monumento al Doctor Robert', 41.39496839, 2.17557192),
+(27, 'Plaza de Nuestra Señora del Pilar', 41.65639299, -0.87892771),
+(29, 'Parc de la Ciutadella', 41.38851322, 2.18581796),
+(30, 'Monument a Colom', 41.37580222, 2.17778206),
+(31, 'El Pez Olímpico', 41.38617889, 2.19705105),
+(32, 'Plaça de Catalunya', 41.38703214, 2.17001438),
+(33, 'Plaça Espanya', 41.37505349, 2.14911461),
+(34, 'Parlament de Catalunya', 41.38812685, 2.18894005);
 
 -- --------------------------------------------------------
 
@@ -211,6 +242,24 @@ ALTER TABLE `Books`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indices de la tabla `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `graphics_barchart`
+--
+ALTER TABLE `graphics_barchart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `graphics_piechart`
+--
+ALTER TABLE `graphics_piechart`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `locations`
 --
 ALTER TABLE `locations`
@@ -239,13 +288,31 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT de la tabla `Books`
 --
 ALTER TABLE `Books`
-  MODIFY `id_book` int(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id_book` int(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+
+--
+-- AUTO_INCREMENT de la tabla `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT de la tabla `graphics_barchart`
+--
+ALTER TABLE `graphics_barchart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `graphics_piechart`
+--
+ALTER TABLE `graphics_piechart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `RecoveryTokens`
@@ -257,7 +324,7 @@ ALTER TABLE `RecoveryTokens`
 -- AUTO_INCREMENT de la tabla `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `id_user` int(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `id_user` int(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
 -- Restricciones para tablas volcadas
--- inprocode/README.md
+++ inprocode/README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Dependencies
+
+npm install chart.js ng2-charts
+
+npm install @fullcalendar/angular @fullcalendar/daygrid @fullcalendar/interaction   
